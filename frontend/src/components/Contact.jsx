e663257c94c28dc31e0ff2e6328627800ae4e49c
Mateusz Wawrzonkowski <mateusz.wawrzonkowski@primaveraperfum.pl>
2021-09-18 21:00:54 +0200

    add contact

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -10,24 +10,28 @@ const ResizeMap = () => {
 
 export default function Contact() {
   return (
-    <div id="mapid">
-      <MapContainer
-        center={[52.249, 20.98]}
-        zoom={13}
-        scrollWheelZoom={false}
-        style={{ height: "450px", width: "100%" }}
-      >
-        <ResizeMap />
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[52.249, 20.98]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </div>
+    <>
+      <div id="mapid">
+        <MapContainer
+          center={[52.249, 20.98]}
+          zoom={13}
+          scrollWheelZoom={false}
+          style={{ height: "450px", width: "100%" }}
+        >
+          <ResizeMap />
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[52.249, 20.98]}>
+            <Popup>Nasza siedziba</Popup>
+          </Marker>
+        </MapContainer>
+        <div className="p-d-flex p-flex-column p-ai-center p-jc-center p-mt-5">
+          <span>Ulica Spacerowa 3, Warszawa, 03-783</span>
+          <span>Numer telefonu: 833-854-111</span>
+        </div>
+      </div>
+    </>
   );
 }
